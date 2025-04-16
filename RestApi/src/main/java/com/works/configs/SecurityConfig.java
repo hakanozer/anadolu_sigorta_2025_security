8e5fb398e83c077fecf271037f08947d87cf151a
Hakan ÖZER <hakanozer02@gmail.com>
2025-04-16 10:29:15 +0300

    Security

--- RestApi/src/main/java/com/works/configs/SecurityConfig.java
+++ RestApi/src/main/java/com/works/configs/SecurityConfig.java
@@ -1,22 +1,44 @@
 package com.works.configs;
 
+import com.works.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final PasswordEncoder passwordEncoder;
+    private final CustomerService customerService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests( req ->
+                        req
+                        .requestMatchers("/product/**").hasRole("product")
+                        .requestMatchers("/note/**").hasRole("note")
+                        .requestMatchers("/customer/**").permitAll()
+                )
                 .csrf(csrf -> csrf.disable())
                 .formLogin(formLogin -> formLogin.disable())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(customerService);
+        return daoAuthenticationProvider;
     }
 
 }
--- RestApi/src/main/java/com/works/services/CustomerService.java
+++ RestApi/src/main/java/com/works/services/CustomerService.java
@@ -1,24 +1,65 @@
 package com.works.services;
 
 import com.works.entities.Customer;
+import com.works.entities.Role;
 import com.works.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @RequestMapping("customer")
-public class CustomerService {
+@Transactional
+public class CustomerService implements UserDetailsService {
 
     private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Customer> customer = customerRepository.findByUsernameEqualsIgnoreCase(username);
+        if (customer.isPresent()) {
+            Customer c = customer.get();
+            return new User(
+                    c.getUsername(),
+                    c.getPassword(),
+                    c.getEnable(),
+                    true,
+                    true,
+                    true,
+                    parseRoles(c.getRoles())
+            );
+        }
+        throw new UsernameNotFoundException("User Not Found");
+    }
+
+    private Collection<? extends GrantedAuthority> parseRoles(List<Role> roles) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Role role : roles) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return grantedAuthorities;
+    }
 
     public Customer register(Customer customer){
         String newPassword = passwordEncoder.encode(customer.getPassword());
         customer.setPassword(newPassword);
         return customerRepository.save(customer);
     }
+
 
 }
